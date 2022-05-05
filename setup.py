b3c72032f283d697a77ac01b8f8f0a0c473a0af9
Anderson Marcos Dias Canteli <andersonmdcanteli@gmail.com>
2022-05-05 19:43:16 -0300

    create setup.py

---
+++ setup.py
@@ -0,0 +1,25 @@
+from setuptools import setup, find_packages
+
+with open("README.md", "r") as readme_file:
+    readme = readme_file.read()
+
+requirements = ["ipython>=6", "colorama>=0.4.4", "matplotlib>=3.5.2", "numpy>=1.22.3", "openpyxl>=3.0.9", "pandas>=1.4.2",
+                "scipy>=1.8.0", "statsmodels>=0.13.2", "tabulate>=0.8.9"
+]
+
+setup(
+    name="pycafee",
+    version="0.1",
+    author="Anderson Marcos Dias Canteli",
+    author_email="andersonmdcanteli@gmail.com",
+    description="A package to make life easy",
+    long_description=readme,
+    long_description_content_type="text/markdown",
+    url="https://github.com/pycafee/pycafee",
+    packages=find_packages(),
+    install_requires=requirements,
+    classifiers=[
+        "Programming Language :: Python :: 3.8",
+        "License :: OSI Approved :: GNU General Public License v3 (GPLv3)",
+    ],
+)
