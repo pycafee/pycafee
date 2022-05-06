e1e4e6695f55956caa69fca16ca077e5350a9593
Anderson Marcos Dias Canteli <andersonmdcanteli@gmail.com>
2022-05-05 23:14:52 -0300

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,11 +1,16 @@
 from setuptools import setup, find_packages
-
 import pathlib
 
-here = pathlib.Path(__file__).parent.resolve()
+# To use a consistent encoding
+from codecs import open
+from os import path
+
+# The directory containing this file
+HERE = path.abspath(path.dirname(__file__))
 
 # Get the long description from the README file
-long_description = (here / "README.md").read_text(encoding="utf-8")
+with open(path.join(HERE, 'README.md'), encoding='utf-8') as f:
+    long_description = f.read()
 
 requirements = ["ipython>=6", "colorama>=0.4.4", "matplotlib>=3.5.2", "numpy>=1.22.3", "openpyxl>=3.0.9", "pandas>=1.4.2",
                 "scipy>=1.8.0", "statsmodels>=0.13.2", "tabulate>=0.8.9"
@@ -13,7 +18,7 @@ requirements = ["ipython>=6", "colorama>=0.4.4", "matplotlib>=3.5.2", "numpy>=1.
 
 setup(
     name="pycafee",
-    version="0.1",
+    version="0.0.1",
     author="Anderson Marcos Dias Canteli",
     author_email="andersonmdcanteli@gmail.com",
     description="A package to make life easy",
@@ -28,5 +33,6 @@ setup(
         "Programming Language :: Python :: 3 :: Only",
         "License :: OSI Approved :: GNU General Public License v3 (GPLv3)",
     ],
+    packages=["pycafee"],
     keywords="statistics, data analisys, sample",  # Optional
 )
