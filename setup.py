036daeb00db92822becd139361cec7a9d42c1a28
Anderson Marcos Dias Canteli <andersonmdcanteli@gmail.com>
2022-05-06 12:00:54 -0300

    update versions

--- setup.py
+++ setup.py
@@ -12,8 +12,8 @@ HERE = path.abspath(path.dirname(__file__))
 with open(path.join(HERE, 'README.md'), encoding='utf-8') as f:
     long_description = f.read()
 
-requirements = ["colorama>=0.4.4", "matplotlib>=3.5.2", "numpy>=1.21.0", "openpyxl>=3.0.9", "pandas>=1.4.2",
-                "scipy>=1.7.0", "statsmodels>=0.13.2", "tabulate>=0.8.9"
+requirements = ["colorama>=0.4.4", "matplotlib>=3.2.2", "numpy>=1.21.0", "openpyxl>=3.0.9", "pandas>=1.3.5",
+                "scipy>=1.4.1", "statsmodels>=0.10.2", "tabulate>=0.8.9"
 ]
 
 setup(
