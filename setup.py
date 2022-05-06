3fdb360f8864927fda1b16af3cfd5ad13700578d
Anderson Marcos Dias Canteli <andersonmdcanteli@gmail.com>
2022-05-06 10:44:24 -0300

    add travis

--- setup.py
+++ setup.py
@@ -12,13 +12,13 @@ HERE = path.abspath(path.dirname(__file__))
 with open(path.join(HERE, 'README.md'), encoding='utf-8') as f:
     long_description = f.read()
 
-requirements = ["ipython>=6", "colorama>=0.4.4", "matplotlib>=3.5.2", "numpy>=1.22.3", "openpyxl>=3.0.9", "pandas>=1.4.2",
-                "scipy>=1.8.0", "statsmodels>=0.13.2", "tabulate>=0.8.9"
+requirements = ["colorama>=0.4.4", "matplotlib>=3.5.2", "numpy>=1.22.3", "openpyxl>=3.0.9", "pandas>=1.4.2",
+                "scipy>=1.7.0", "statsmodels>=0.13.2", "tabulate>=0.8.9"
 ]
 
 setup(
     name="pycafee",
-    version="0.0.1",
+    version="0.0.2",
     author="Anderson Marcos Dias Canteli",
     author_email="andersonmdcanteli@gmail.com",
     description="A package to make scientific researchers easy",
